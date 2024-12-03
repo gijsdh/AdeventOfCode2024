e7f0ffd4a64d88f760f915eae6b760b3d152485d
Gijs den Hollander <gijs.den.hollander@ing.com>
2024-12-03 07:16:46 +0100

    using groups in regex

--- src/main/kotlin/event2024/day3.kt
+++ src/main/kotlin/event2024/day3.kt
@@ -1,19 +1,23 @@
 fun main(args: Array<String>) {
     val input = getResourceAsText("input.txt")
 
-    val regex = "mul\\(\\d{0,3},\\d{0,3}\\)".toRegex()
+    // You can group values by surrounding them with parenthesis. => (\d{0,3})
+    // So the result is 3 groups = [mul(427,266), 427, 266]
+    // This saves some time getting the numbers out.
+    val regex = "mul\\((\\d{0,3}),(\\d{0,3})\\)".toRegex()
+
     val matches = regex.findAll(input, 0)
     println("Solution part 1 ${calculateSum(matches)}")
 
 
-    val regexPart2 = "mul\\(\\d{0,3},\\d{0,3}\\)|do\\(\\)|don't\\(\\)".toRegex()
+    val regexPart2 = "mul\\((\\d{0,3}),(\\d{0,3})\\)|do\\(\\)|don't\\(\\)".toRegex()
     val matchesPart2 = regexPart2.findAll(input, 0)
     println("Solution part 2 ${calculateSum(matchesPart2)}")
 }
 
 private fun calculateSum(matchesPart2: Sequence<MatchResult>): Int {
     var stop = false
-    var sum2 = 0
+    var sum = 0
     for (match in matchesPart2) {
         val value = match.value
         if (value == "do()") {
@@ -24,10 +28,9 @@ private fun calculateSum(matchesPart2: Sequence<MatchResult>): Int {
             stop = true
             continue
         }
-        val split = value.split("mul(", ",", ")")
-        sum2 += split[1].toInt() * split[2].toInt()
+        sum += match.groupValues[1].toInt() * match.groupValues[2].toInt()
     }
-    return sum2
+    return sum
 }
 
 
