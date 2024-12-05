c331d4a66a83ce2fe35dde3db5bf81058472d599
Gijs den Hollander <gijs.den.hollander@ing.com>
2024-12-05 07:52:27 +0100

    some clean up

--- src/main/kotlin/event2025/day5.kt
+++ src/main/kotlin/event2025/day5.kt
@@ -6,60 +6,61 @@ fun main(args: Array<String>) {
             .map { it.trim() }
         }
 
-
-    var map : MutableMap<Int, List<Int>> = mutableMapOf();
+    val constraints: MutableMap<Int, List<Int>> = mutableMapOf();
     for (line in inputLines[0]) {
-        var numbers = line.split("|").map { it.toInt() }
-        map.merge(numbers.first(), listOf(numbers.last()), List<Int>::plus)
+        val numbers = line.split("|").map { it.toInt() }
+        constraints.merge(numbers[0], listOf(numbers[1]), List<Int>::plus)
     }
 
+    val incorrectOrder = mutableSetOf<MutableList<Int>>()
     var sum = 0
-    var incorrectOrder = mutableListOf<MutableList<Int>>()
+
     for (line in inputLines[1]) {
+        // parse list.
         var numbers = line.split(",").map { it.toInt() }
         var checkValid = true
+
+        // So going through the list, we check if there are constraints on that number.
+        // If there are constraints we check if those are not before our current number in the list.
         for (number in numbers.withIndex()) {
-            if (map.contains(number.value)) {
-                for (checks in map[number.value]!!) {
-                    if (numbers.subList(0, number.index).contains(checks)) {
-                        checkValid = false
-                        incorrectOrder.add(numbers.toMutableList())
-                        break
-                    }
+            for (constraint in constraints[number.value]?: emptyList()) {
+                if (numbers.subList(0, number.index).toSet().contains(constraint)) {
+                    checkValid = false
+                    incorrectOrder.add(numbers.toMutableList())
+                    break
                 }
-                if (!checkValid) break
             }
         }
+        // apparently all list have an uneven size.
         if (checkValid) sum += numbers[numbers.size / 2]
     }
     println("Solution part 1 ${sum}")
 
+    // Underneath we do the same check as above, but if it fails we move the constraint number after the number we are checking.
+    // TODO look into topological sorting.
     var sum2 = 0
     for (numbers in incorrectOrder) {
-        var work =numbers
         while (true) {
             var checkValid = true
             var swap = -1
-            for (number in work.withIndex()) {
-                if (map.contains(number.value)) {
-                    for (checks in map[number.value]!!) {
-                        if (work.subList(0, number.index).contains(checks)) {
-                            swap = checks
-                            checkValid=false
-                            break
-                        }
+            for (number in numbers.withIndex()) {
+                for (constraint in constraints[number.value]?: emptyList()) {
+                    if (numbers.subList(0, number.index).contains(constraint)) {
+                        swap = constraint
+                        checkValid = false
+                        break
                     }
                 }
                 if (!checkValid) {
-                    work.remove(swap)
-                    work.add(number.index, swap)
+                    numbers.remove(swap)
+                    numbers.add(number.index, swap)
                     break
                 }
             }
             if (checkValid) break
         }
 
-        sum2 += work[work.size / 2]
+        sum2 += numbers[numbers.size / 2]
     }
 
     println("Solution part 2 ${sum2}")
